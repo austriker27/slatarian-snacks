07e39ff535146beb85f737b6024d31f5e1bbe4ca
David Lindahl <David.Lindahl@indigoslate.com>
2018-09-27 15:12:12 -0700

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Slatarian Snacks</title>
+</head>
+<body>
+    <h1>Slatarian Snacks</h1>
+    <ul>
+        <?php foreach ($snacks as $snack) : ?>
+            <li><?=$task; ?></li>
+        <?php endforeach; ?>
+    
+    </ul>
+</body>
+</html>
