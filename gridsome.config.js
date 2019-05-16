8b4fd9bdb367bc53c469cb2258d0ab9b8e58ec79
austriker27 <davidalindahl@gmail.com>
2019-05-16 11:22:33 -0700

    finally got tailwind and gridsome to play together

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,71 @@
-// This is where project configuration and plugin options are located. 
+// // This is where project configuration and plugin options are located.
+// // Learn more: https://gridsome.org/docs/config
+
+// // Changes here require a server restart.
+// // To restart press CTRL + C in terminal and run `gridsome develop`
+
+// const tailwindcss = require('tailwindcss');
+// const purgecss = require('@fullhuman/postcss-purgecss');
+
+// const postcssPlugins = [tailwindcss()];
+
+// if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss());
+
+// module.exports = {
+//   siteName: 'Slatarian Snacks',
+//   siteDescription: 'Snacks for Slatarians',
+//   siteUrl: 'https://slatariansnacks.netlify.com',
+//   icon: './src/favicon.png',
+//   css: {
+//     loaderOptions: {
+//       postcss: {
+//         plugins: postcssPlugins
+//       }
+//     }
+//   }
+// };
+
+// This is where project configuration and installed plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: 'Snacks for Slatarians',
+  siteUrl: 'https://slatariansnacks.netlify.com',
+  siteDescription: 'Snacks for Slatarians',
+
+  chainWebpack: config => {
+    config.module
+      .rule('css') // css, sass, scss, less, postcss, stylus
+      .oneOf('normal') // normal, module
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(
+          ...[
+            require('postcss-import'),
+            require('tailwindcss')('./tailwind.config.js'),
+            require('postcss-preset-env')({
+              stage: 0,
+              autoprefixer: false
+              // features: {
+              //   'nesting-rules': {
+              //     bubble: ['apply', 'variants', 'responsive', 'screen'],
+              //   },
+              // },
+            })
+          ]
+        );
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(...[require('@fullhuman/postcss-purgecss')]);
+        }
+
+        return options;
+      });
+
+    //   config.module
+    //     .rule('vue')
+    //     .use('vue-svg-inline-loader')
+    //     .loader('vue-svg-inline-loader')
+    //     .options({ /* ... */ })
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Lazer', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'indigo-blue': '#171931'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
